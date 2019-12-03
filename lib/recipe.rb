3b87fa3e453f9bc9cfa64dc7a2d22d84eae8b6ff
Andrew Luchuk <andrew.luchuk@outlook.com>
2019-12-03 17:16:42 -0500

    corrects belongs_to relationship for User and Recipe

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,8 +1,8 @@
 class Recipe < ActiveRecord::Base
     has_many :meals
     has_many :users, through: :meals
-    has_many :user_recipes
-    has_many :users, through: :user_recipes
+    # has_many :user_recipes
+    # has_many :users, through: :user_recipes
     has_many :recipe_ingredients
     has_many :ingredients, through: :recipe_ingredients
     belongs_to :user
--- lib/user.rb
+++ lib/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
     has_many :meals
-    has_many :recipes, through: :recipes
-    has_many :user_recipes
-    has_many :recipes, through: :user_recipes
-    has_many :recipes
+    has_many :recipes, through: :meals
+    # has_many :user_recipes
+    # has_many :recipes, through: :user_recipes
+    # alias_attribute :recipes, :created_recipes
+    has_many :created_recipes, :class_name => "Recipe"
 end
