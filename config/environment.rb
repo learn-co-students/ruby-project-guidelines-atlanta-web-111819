0081ae029038f96293d27599e5418fe661689205
RobertTTaylor9517 <robt15@hotmail.com>
2019-12-03 15:47:07 -0500

    added rbs

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,10 @@
-require 'bundler'
+require 'bundler/setup'
 Bundler.require
 
-ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
+ActiveRecord::Base.establish_connection(
+    adapter: 'sqlite3', 
+    database: 'db/development.db'
+    )
+ActiveRecord::Base.Logger.new(STDOUT)
+
 require_all 'lib'
