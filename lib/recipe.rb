00527295bc53159f07de2c0389a169d674038570
Andrew Luchuk <andrew.luchuk@outlook.com>
2019-12-03 14:44:25 -0500

    adds Ingredient relationships

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -3,4 +3,6 @@ class Recipe < ActiveRecord::Base
     has_many :users, through: :meals
     has_many :user_recipes
     has_many :users, through: :user_recipes
+    has_many :recipe_ingredients
+    has_many :ingredient_id, through: :recipe_ingredients
 end
