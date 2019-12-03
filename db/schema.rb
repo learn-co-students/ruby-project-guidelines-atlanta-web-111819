660ca1c0be87c3f124e33977025b655e9123f993
Andrew Luchuk <andrew.luchuk@outlook.com>
2019-12-03 14:22:53 -0500

    adds UserRecipe to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191203191929) do
+ActiveRecord::Schema.define(version: 20191203192203) do
 
   create_table "categories", force: :cascade do |t|
     t.string "title"
@@ -44,6 +44,11 @@ ActiveRecord::Schema.define(version: 20191203191929) do
   create_table "user_categories", force: :cascade do |t|
     t.integer "category_id"
     t.integer "user_id"
+  end
+
+  create_table "user_recipes", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "recipe_id"
   end
 
   create_table "users", force: :cascade do |t|
