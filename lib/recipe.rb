b1fbaddbba3bd56a8a14d8a2010518e8d7b57a2a
Andrew Luchuk <andrew.luchuk@outlook.com>
2019-12-03 14:40:55 -0500

    adds User relationships

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,3 +1,6 @@
 class Recipe < ActiveRecord::Base
-    
+    has_many :meals
+    has_many :users, through: :meals
+    has_many :user_recipes
+    has_many :users, through: :user_recipes
 end
--- lib/user.rb
+++ lib/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
-    
+    has_many :meals
+    has_many :recipes, through: :recipes
+    has_many :user_recipes
+    has_many :recipes, through: :user_recipes
 end
