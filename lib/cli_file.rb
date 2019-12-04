d671bc85b86e8ff65e2de50b427add929be1d6de
RobertTTaylor9517 <robt15@hotmail.com>
2019-12-04 17:54:36 -0500

    day 1 almost finished

--- lib/cli_file.rb
+++ lib/cli_file.rb
@@ -18,7 +18,7 @@ def get_characters
 
     while looper == "y"
         puts "\n\n"
-        puts "next, last, back to menu?"
+        puts "next, last, add, back to menu?"
         choice = get_entry
 
         if choice == "next"
@@ -27,6 +27,8 @@ def get_characters
         elsif choice == "last" && offset_num >= 100
             offset_num -= 200
             print_character(fetch_marvel("characters", offset_num))
+        elsif choice == "add"
+            add_to_db("characters", offset_num - 100)
         elsif choice == "back to menu"
             looper = "n"
         else
@@ -54,6 +56,8 @@ def get_comics
         elsif choice == "last" && offset_num >= 100
             offset_num -= 200
             print_comic(fetch_marvel("comics", offset_num))
+        elsif choice == "add"
+            add_to_db("comics", offset_num - 100)
         elsif choice == "back to menu"
             looper = "n"
         else
@@ -78,3 +82,30 @@ end
 def get_entry
     gets.chomp
 end
+
+def add_to_db(whch, offset_num)
+    temp = fetch_marvel(whch, offset_num)
+
+    puts "Which would you like to add? or add all?"
+    choice2 = get_entry
+    if choice2 == "add all"
+        temp["data"]["results"].each do |char|
+            if whch == "characters"
+                Character.find_or_create_by(char_id: char["id"], name: char["name"], desc: char["description"])
+            elsif whch == "comics"
+                Comic.find_or_create_by(comic_id: char["id"], name: char["title"], release_date: char["dates"][0]["date"], issue_num: char["issueNumber"], description: char["description"])
+            end
+        end
+    else
+        if whch == "characters"
+            tmpchar = temp["data"]["results"].select{|item| item["name"] == choice2}
+            binding.pry
+            Character.find_or_create_by(char_id: tmpchar[0]["id"], name: tmpchar[0]["name"], desc: tmpchar[0]["description"])
+        elsif whch == "comics"
+            tmpchar = temp["data"]["results"].select{|item| item["title"] == choice2}
+            Comic.find_or_create_by(comic_id: tmpchar[0]["id"], name: char["title"], release_date: tmpchar[0]["dates"][0]["date"], issue_num: tmpchar[0]["issueNumber"], description: tmpchar[0]["description"])
+        end
+    end
+                
+end
+
