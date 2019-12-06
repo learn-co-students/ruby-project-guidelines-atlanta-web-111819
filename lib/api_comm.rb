a013f782120324fa9ed7cb1bde26a38068282ecc
RobertTTaylor9517 <robt15@hotmail.com>
2019-12-06 11:46:43 -0500

    n

--- lib/api_comm.rb
+++ lib/api_comm.rb
@@ -3,38 +3,35 @@ require 'json'
 require 'pry'
 require 'digest'
 
+PUBLIC_KEY = ENV["PUBLIC_KEY"]
+PRIVATE_KEY = ENV["PRIVATE_KEY"]
+
 def fetch_marvel(entry, offset = 0)
-  public_key = ENV["PUBLIC_KEY"]
-  private_key = ENV["PRIVATE_KEY"]
   time = Time.now
-  hash_soup = time.to_s + private_key + public_key
+  hash_soup = time.to_s + PRIVATE_KEY + PUBLIC_KEY
   hash = Digest::MD5.hexdigest hash_soup
-  # url_mine = "https://gateway.marvel.com/v1/public/characters?ts=#{time.to_s}&apikey=#{public_key}&hash=#{hash}"
-  response = RestClient.get("https://gateway.marvel.com/v1/public/#{entry}?ts=#{time.to_s}&apikey=#{public_key}&hash=#{hash}&limit=100&offset=#{offset}")
+  # url_mine = "https://gateway.marvel.com/v1/public/characters?ts=#{time.to_s}&apikey=#{PUBLIC_KEY}&hash=#{hash}"
+  response = RestClient.get("https://gateway.marvel.com/v1/public/#{entry}?ts=#{time.to_s}&apikey=#{PUBLIC_KEY}&hash=#{hash}&limit=100&offset=#{offset}")
   marv_response = JSON.parse(response)
 
   return marv_response
 end
 
 def fetch_comics(id_num)
-  public_key = "5e8946f12e3dfaf42d0e54483983358e"
-  private_key = "a449d1ddb8da13dfbd329ff8dc0d9747375a63e9"
   time = Time.now
-  hash_soup = time.to_s + private_key + public_key
+  hash_soup = time.to_s + PRIVATE_KEY + PUBLIC_KEY
   hash = Digest::MD5.hexdigest hash_soup
-  response = RestClient.get("http://gateway.marvel.com/v1/public/characters/#{id_num}/comics?ts=#{time.to_s}&apikey=#{public_key}&hash=#{hash}")
+  response = RestClient.get("http://gateway.marvel.com/v1/public/characters/#{id_num}/comics?ts=#{time.to_s}&apikey=#{PUBLIC_KEY}&hash=#{hash}")
   marv_response = JSON.parse(response)
 
   return marv_response
 end
 
 def fetch_characters(id_num)
-  public_key = "5e8946f12e3dfaf42d0e54483983358e"
-  private_key = "a449d1ddb8da13dfbd329ff8dc0d9747375a63e9"
   time = Time.now
-  hash_soup = time.to_s + private_key + public_key
+  hash_soup = time.to_s + PRIVATE_KEY + PUBLIC_KEY
   hash = Digest::MD5.hexdigest hash_soup
-  response = RestClient.get("http://gateway.marvel.com/v1/public/comics/#{id_num}/characters?ts=#{time.to_s}&apikey=#{public_key}&hash=#{hash}")
+  response = RestClient.get("http://gateway.marvel.com/v1/public/comics/#{id_num}/characters?ts=#{time.to_s}&apikey=#{PUBLIC_KEY}&hash=#{hash}")
   marv_response = JSON.parse(response)
 
   return marv_response
